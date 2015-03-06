76d869bf0a3e74c7bb192a18bc0d1533c464e3dd
darul75 <darul75@gmail.com>
2015-03-06 09:32:20 +0100

    refactoring

--- test/library/conf.js
+++ test/library/conf.js
@@ -49,7 +49,7 @@ module.exports = function(config) {
     // - PhantomJS
     // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
     // browsers: ['Chrome', 'PhantomJS'],
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // If browser does not capture in given timeout [ms], kill it
@@ -58,6 +58,6 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
-    singleRun: false
+    singleRun: true
   });
 };
--- test/library/librarySpec.js
+++ test/library/librarySpec.js
@@ -60,7 +60,33 @@ describe('angular-library', function () {
   // SERVICE TESTING
   describe('books services', function () {
     it('testing factory', function() {
+      var offers = [
+        {"type": "percentage", "value": 5},
+        {"type": "minus", "value": 15},
+        {"type": "slice", "sliceValue": 100, "value": 12}
+      ];
+
+      var books = [
+        {
+          "isbn": "c8fabf68-8374-48fe-a7ea-a00ccd07afff",
+          "title": "Henri Potier à l'école des sorciers",
+          "price": 35,
+          "cover": "http://henri-potier.xebia.fr/hp0.jpg"
+        },
+        {
+          "isbn": "a460afed-e5e7-4e39-a39d-c885c05db861",
+          "title": "Henri Potier et la Chambre des secrets",
+          "price": 30,
+          "cover": "http://henri-potier.xebia.fr/hp1.jpg"
+        }
+      ];
       
+      // minus
+      expect(booksOffersFactory.computeTotalPrice(books, 65, offers)).toBe(50);
+            
+      // percentage
+      offers[1].value = 0;
+      expect(booksOffersFactory.computeTotalPrice(books, 65, offers)).toBe(62);
     });    
   });
 
